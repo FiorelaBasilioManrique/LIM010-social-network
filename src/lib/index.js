32e366ab13b232a25f54ab8e17accc139c6dad73
Fiorela <fbasiliomanrique@gmail.com>
2019-09-03 18:25:17 -0500

    Realizando las publicaciones con HTML, CSS y JS

--- src/lib/index.js
+++ src/lib/index.js
@@ -12,6 +12,8 @@ const ingresar = document.getElementById('ingresar');
 const publicaciones = document.getElementById('publicaciones');
 const fondo = document.getElementById('fondo');
 const mensaje = document.getElementById('mensaje');
+const encabezado = document.getElementById('encabezado');
+const publicar = document.getElementById('publicar');
 
 // Almacenar datos 
 let datos = [];
@@ -73,8 +75,11 @@ registro.addEventListener('click', (e) => {
         fondo.classList.add('hide');
         login.classList.add('hide');
         publicaciones.classList.remove('hide');
+        encabezado.classList.remove('hide');
+        publicar.classList.remove('hide');
 
         bienvenida.innerHTML = datosLocal[i].user;
+        bienvenidaDos.innerHTML = datosLocal[i].user;
         bienvenidaCorreo.innerHTML = datosLocal[i].email;
       } else {
         mensaje.innerHTML= 'correo o contraseña incorrectos';
@@ -83,120 +88,27 @@ registro.addEventListener('click', (e) => {
 });
 
 
+// PUBLICACIONES
 
+// const name;
+// const nombres = [];
+// const name2;
+// const userTR = document.getElementById('nameTR');
+// const form = document.getElementById('form');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const login = document.getElementById('login');
-// const segundaPantalla = document.getElementById('segundaPantalla');
-// const registroCuenta = document.getElementById('registroCuenta');
-// const error = document.getElementById('error');
-// const registrar = document.getElementById('registrar');
-// const publicacion = document.getElementById('publicacion');
-// const registro = document.getElementById('registro');
-
-
-// // Almacendo datos
-// let datos = [];
-
-// // Dirigirse a la ventana registrarse
-// registroCuenta.addEventListener('click', ()=>{
-//     login.classList.add('hide');
-//     segundaPantalla.classList.remove('hide');
+// form.addEventListener('click', (e)=>{
+//   e.preventDefault();
 // });
 
-// // Validar correo
-// // const correoCorrecto = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-// const validarCorreo = (correo) => {
-//     const correoCorrecto = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-//     return correoCorrecto.test(correo);
-// }
 
-// // Comenzando con el registro
-// registro.addEventListener('click', (e)=>{
-//   e.preventDefault();
-//     const nombre = document.getElementById('nombre').value;
-//     const correo = document.getElementById('correo').value;
-//     const password = document.getElementById('password').value;
 
-//     // if(nombre === '' || correo === '' || password === '' ){
-//     //   alert('Todos los campos son obligatorios');
-//     //   return false;
-//     // } else if (!correoCorrecto.test(correo)) {
-//     //   alert('El correo no es valido')
-//     //   return false;
-//     // } else if (nombre.length >20 || password.length >=8) {
-//     //   alert('El correo es correcto'); 
-//     //   return false;
-//     // }
 
-//     if(validarCorreo(correo.value) && password.value.length >=8){
 
-//       datos.push({
-//         user: nombre.value,
-//         email: correo.value,
-//         nuevoPassword: password.value,
-//       });
 
-//       // Limpiar
 
-//       nombre = '';
-//       correo= '';
-//       password= '';
 
-//       console.log(datos);
-      
-//       localStorage.setItem('datos', JSON.stringify(datos));
-      
-//       alert('El correo es correcto'); 
 
-//       segundaPantalla.classList.add('hide');
-//       login.classList.add('hide');
 
-//       // localStorage.setItem('nombre', nombre);
-//       // localStorage.setItem('correo', correo);
-//       // localStorage.setItem('password', password);
-  
-//     } else {
-//      error.innerHTML='error correo no valido';
-     
-//     }
-  
-  
-//     // if(password.length <= 5 || password.length >= 10){
-//     //   error.innerHTML=('error la contraseña debe tene 6 y 10 caracteres');
-  
-//     // }
-//     // if(nombre === ''){
-//     //   error.innerHTML='Completa el nombre';
-//     // }
-//   });
-  
-//   const ingresar = document.getElementById('ingresar');
-  
-//   ingresar.addEventListener('click', ()=>{
-//     const correo = document.getElementById('correo').value;
-//     const password = document.getElementById('password').value;
-  
-//     alert('bienvenidas');
-//   });
+
+
+
